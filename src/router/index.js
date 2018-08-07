25b065cc18715afdf0ff730a9c3fe10beb26a262
kiikura <keniikura@gmail.com>
2018-08-07 16:57:38 +0900

    vue-router config for 4 pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import top from '@/components/top';
+import login from '@/components/login';
+import vote from '@/components/vote';
+import admin from '@/components/admin';
+import count from '@/components/count';
 
 Vue.use(Router);
 
@@ -11,6 +15,26 @@ export default new Router({
       path: '/',
       name: 'top',
       component: top,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: login,
+    },
+    {
+      path: '/vote',
+      name: 'Vote',
+      component: vote,
+    },
+    {
+      path: 'admin',
+      name: 'Admin',
+      component: admin,
+    },
+    {
+      path: '/count',
+      name: 'Count',
+      component: count,
     },
     {
       path: '/hello',
